ca1e14e72c6b4c170410a9fcd4562cf02f79cb4d
sshynar <shsagnayeva@gmail.com>
2022-09-01 07:46:55 -0700

    iteration1 AB#380

--- src/com/game/TitlePage.java
+++ src/com/game/TitlePage.java
@@ -15,7 +15,7 @@ class TitlePage {
     public static void title() throws InterruptedException {
         for (int i = 0; i < art.length(); i++){
             System.out.print(art.charAt(i));
-            Thread.sleep(2);
+//            Thread.sleep(2);
         }
         System.out.println("\nWelcome to Key Quest!");
     }
