58897cba8a690d76c67e4f3d72154529cc0e6645
sshynar <shsagnayeva@gmail.com>
2022-08-31 17:49:30 -0700

    iteration1 AB#380

--- src/com/game/GameClient.java
+++ src/com/game/GameClient.java
@@ -22,7 +22,6 @@ public class GameClient {
 
         while (true) {
             String firstCommand = GameManager.start();
-
             if (Objects.equals(firstCommand, "quit")) {
                 GameManager.quit();
                 break;
@@ -49,6 +48,7 @@ public class GameClient {
                     }
 
                     if (isValidVerb && isValidLocation) {
+
                         JSONArray nextCommandsJsonArray = jsonObjectCommand.getJSONArray(phrase[0]);
                         JSONArray nextLocationsJsonArray = jsonObjectLocation.getJSONArray(currentLocation);
                         String[] nextCommands = getStringArray(nextCommandsJsonArray);
@@ -57,7 +57,6 @@ public class GameClient {
                         for (String nextLocation : nextLocations) {
                             if (Arrays.asList(nextLocations).contains(phrase[1]) && (Arrays.asList(nextCommands).contains(phrase[1]))) {
                                 currentLocation = phrase[1];
-                                //System.out.println("New location: " + currentLocation);
                                 break;
                             } else if (phrase[1].equals(currentLocation)) {
                                 System.out.println("Already in " + phrase[1]);
@@ -68,82 +67,19 @@ public class GameClient {
                             } else if (!Arrays.asList(nextCommands).contains(phrase[1])) {
                                 System.out.println("Cannot " + phrase[0] + " " + phrase[1]);
                                 break;
-                            } else if (phrase[0] == "quit") {
-                                GameManager.quit();
                             }
                         }
-                    }  else {
+                    } else if (Objects.equals(phrase[0], "quit")) {
+                        String confirmation = GameManager.confirmQuit();
+                        if(Objects.equals(confirmation, "yes")) {
+                            GameManager.quit();
+                            break;
+                        }
+                    } else {
                         System.out.println("Please try another command");
                     }
                 } while (!Objects.equals(phrase[0], "quit"));
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-/*        String currentLocation = "foyer";
-        System.out.println("Current location is " + currentLocation);
-        JSONObject jsonObjectCommand = JSONParser.ReadJSON("command.json");
-        JSONObject jsonObjectLocation = JSONParser.ReadJSON("location.json");
-
-        Set<String> keysCommand = JSONParser.geyKeys(jsonObjectCommand);
-        System.out.println("List of available commands: " + keysCommand);
-        Set<String> keysLocation = JSONParser.geyKeys(jsonObjectLocation);
-        System.out.println("List of available locations: " + keysLocation);
-
-        String[] phrase = TextParser.parse();
-        for (String s : phrase) {
-            System.out.println(s);
-        }
-
-        boolean isValidVerb = false;
-        boolean isValidLocation = false;
-
-        for (int i = 0; i < phrase.length; i++) {
-            try {
-                isValidVerb = jsonObjectCommand.has(phrase[0]);
-                isValidLocation = jsonObjectLocation.has(phrase[1]);
-            } catch (Exception e) {
-                throw new RuntimeException("Command not recognized");
-            }
-        }
-
-        System.out.println(isValidVerb);
-        System.out.println(isValidLocation);
-
-        if (isValidVerb && isValidLocation) {
-            JSONArray nextCommandsJsonArray = jsonObjectCommand.getJSONArray(phrase[0]);
-            JSONArray nextLocationsJsonArray = jsonObjectLocation.getJSONArray(currentLocation);
-            String[] nextCommands = getStringArray(nextCommandsJsonArray);
-            String[] nextLocations = getStringArray(nextLocationsJsonArray);
-
-            for (String nextLocation : nextLocations) {
-                if (Arrays.asList(nextLocations).contains(phrase[1]) && (Arrays.asList(nextCommands).contains(phrase[1]))) {
-                    currentLocation = phrase[1];
-                    System.out.println("New location: " + currentLocation);
-                    break;
-                } else if (phrase[1].equals(currentLocation)) {
-                    System.out.println("Already in " + phrase[1]);
-                    break;
-                } else if (!phrase[1].equals(nextLocation)) {
-                    System.out.println("Cannot " + phrase[0] + ": " + currentLocation + " ---> " + phrase[1]);
-                    break;
-                } else if (!Arrays.asList(nextCommands).contains(phrase[1])) {
-                    System.out.println("Cannot " + phrase[0] + " " + phrase[1]);
-                    break;
-                }
-            }
-        } else {
-            System.out.println("Please try another command");
-        }*/
     }
 }
--- src/com/game/GameManager.java
+++ src/com/game/GameManager.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class GameManager {
     public static String start() {
-        System.out.println("Would you like to start a game? Enter 'start' to start, 'quit' to exit \n>>> ");
+        System.out.println("Would you like to start the game? Enter 'start' to start, 'quit' to exit \n>>> ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
@@ -14,5 +14,11 @@ public class GameManager {
         System.out.println("............");
         Thread.sleep(2);
         System.out.println("Game closed");
+    }
+
+    public static String confirmQuit() {
+        System.out.println("Confirm exit the game? Enter 'yes' to confirm exit, 'no' to return \n>>> ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
