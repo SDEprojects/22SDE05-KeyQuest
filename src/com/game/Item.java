dd7dcc36f224c36769dc0531cd64533d2ec22ade
Neten Shrestha <netenshrestha@gmail.com>
2022-09-08 17:46:58 -0400

    iteration 2 for "Item" class refactoring in progress, AB#394 date9/8

--- src/com/game/Item.java
+++ src/com/game/Item.java
@@ -4,25 +4,28 @@ import org.json.JSONObject;
 import static com.game.JSONParser.*;
 
 public class Item {
-    private final String name = getItemName() ;
-    private final String room = getItemRoom();
-    private final String furniture= getItemFurniture();
-    private final String usage= getItemUsage();
+    private final String name;// = getItemName() ;
+    private final String room;// = getItemRoom();
+    private final String furniture;//= getItemFurniture();
+    private final String usage;//= getItemUsage();
 
    /* JSONObject jsonObjectItem = JSONParser.ReadJSON("items.json");
     JSONObject items = jsonObjectItem.getJSONObject("item");*/
 
-  /*  public Item(String name){
-    this.name = name;
-    String roomKey = "room";
+    public Item(String name){
+        this.name = name;
+    /*String roomKey = "room";
     String furnitureKey = "furniture";
     String usageKey = "usage";
     JSONObject information = items.getJSONObject(name);
     room = information.getString(roomKey);
     furniture = information.getString(furnitureKey);
-    usage = information.getString(usageKey);
+    usage = information.getString(usageKey);*/
+        room =getItemRoom();
+        furniture=getItemFurniture();
+        usage= getItemUsage();
         //System.out.println(room);
-    }*/
+    }
 
     public String getName() {
         return name;
